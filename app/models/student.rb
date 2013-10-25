6c6e468bbca88ddcf425780976ab4de40495be3a
Apprentice <apprentice@dbc06.local>
2013-10-24 18:01:46 -0700

    initial AR Student Schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,25 @@
 require_relative '../../db/config'
+require 'date'
+require 'time'
 
-class Student < ActiveRecord::Base
+
+class Student < ActiveRecord::Base  
+  def name
+    self[:first_name] + " " + self[:last_name]
+  end
+
+  def age
+    Time.now.year - self[:birthday].year
+  end
+
+  def get_phone_number
+    self[:phone].scan(/\d/).join
+  end
+
+  validates :email, :format => { :with => /.*[@].*[.].{2}/ }
+  validates :email, :uniqueness => true 
+  validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+  validates :get_phone_number, :length => { :minimum => 10 }
 # implement your Student model here
+
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,18 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+
+    create_table :students do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :gender
+      t.string  :email
+      t.string  :phone
+      t.date  :birthday
+
+      t.timestamps
+    
     # HINT: checkout ActiveRecord::Migration.create_table
   end
+end
 end
